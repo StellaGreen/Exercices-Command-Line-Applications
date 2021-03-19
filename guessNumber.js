d6c9ba997d0c32fc202655eac3d385f552dfcef3
StellaGreen <vickysoler5@gmail.com>
2021-03-19 08:59:51 +0100

    add correction

--- guessNumber.js
+++ guessNumber.js
@@ -5,17 +5,24 @@ const chalk = require('chalk')
 
 // ---- PAS BESOIN DE LIRE CE CODE, IL EST PAS BON, J'AI PAS TROUVÉ COMMENT FAIRE J'ATTENDS LA CORRECTION POUR COMPRENDRE ----- //
 
-const nb = readlineSync.question('Devine mon chiffre entre 0 et 100, à ton avis ? ')
+const nb = Number(readlineSync.question('Vous êtes le maître du jeux, choisissez un nombre entre 0 et 100 ? '))
 
-if (nb === 63){
-        console.log(chalk.green('Bravos !'))
-        }
-    if(nb !== 63){
-        if (nb >= 64){
-        console.log(chalk.red('Nombre trop grand...'))
-        }
-    
-        if (nb <= 62){
-        console.log(chalk.red('Nombre trop petit...'))
-        } 
-        }
+if( nb > 100){
+    console.log(`Tout nombre au dessus de 100 n'est pas admis dans le jeu, veuillez recommencer.`)
+    process.exit(1)
+}
+
+let notfound = true
+
+while (notfound){
+    let input = Number(readlineSync.question('Devine mon chiffre entre 0 et 100, à ton avis ? '))
+
+if (input === nb){
+    console.log(chalk.green('Bravo!'))
+    notfound = false
+} else {
+    console.log(chalk.red(input > nb ? 'Nombre trop grand' : 'Nombre trop petit'))
+}
+
+}
+
